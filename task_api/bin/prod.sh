365942767024c321864bc4f8f94a2ee4fef81d5f
Volkan Altan <volkanaltan@gmail.com>
2019-03-17 20:20:11 +0300

    fix some issues

--- task_api/bin/prod.sh
+++ task_api/bin/prod.sh
@@ -5,11 +5,11 @@ cd /var/www/symfony
 #cp -R .env.dev .env
 cp -R .env.prod .env
 
-composer install --no-dev --optimize-autoloader
+composer install --optimize-autoloader
+#composer install --no-dev --optimize-autoloader
 bin/console doctrine:database:create
 bin/console doctrine:schema:update --force
-#bin/console doctrine:fixtures:load --append
 
-APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+#APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 
 echo "!!!!!! Everything is ready to use !!!!!!"
