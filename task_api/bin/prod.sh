c0f489689ce6ef90af07d0c9e1145369fad49b9c
Volkan Altan <volkanaltan@gmail.com>
2019-03-17 21:19:50 +0300

    some imp.

--- task_api/bin/prod.sh
+++ task_api/bin/prod.sh
@@ -11,6 +11,6 @@ bin/console doctrine:schema:update --force
 
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 
-curl -X GET "http://nginx/api/v1/tasks" -H "accept: application/json"
-
 echo "!!!!!! Everything is ready to use !!!!!!"
+
+curl -X GET "http://nginx/api/v1/tasks" -H "accept: application/json"
